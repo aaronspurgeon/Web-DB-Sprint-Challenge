ccca8a1a747385bcb9da50efebf2c8cccd04ecf1
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-01-10 19:16:17 -0500

    Created migrations for projects and tasks

--- data/migrations/20200110183704_initial.js
+++ data/migrations/20200110183704_initial.js
@@ -1,3 +1,24 @@
-exports.up = async knex => {};
+exports.up = async knex => {
+  await knex.schema.createTable("projects", table => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.string("description");
+    table.boolean("completed").notNullable();
+  });
+
+  await knex.schema.createTable("tasks", table => {
+    table.increments("id");
+    table.string("description").notNullable();
+    table.string("notes");
+    table.boolean("completed").notNullable();
+    table
+      .integer("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
 
 exports.down = async knex => {};
--- data/seeds/02-tasks.js
+++ data/seeds/02-tasks.js
@@ -4,23 +4,27 @@ exports.seed = async knex => {
       project_id: 1,
       description:
         "I need to create a data model for my API and structure my backend.",
-      notes: "Need to make sure I update my node version!"
+      notes: "Need to make sure I update my node version!",
+      completed: false
     },
     {
       project_id: 1,
       description:
-        "Next I need to set up redux and get data storage set up properly on my app."
+        "Next I need to set up redux and get data storage set up properly on my app.",
+      completed: false
     },
     {
       project_id: 2,
       description: "I have to set up my Firebase storage and authentication.",
       notes:
-        "It will also help to solidify my knowledge of Firebase a bit more!"
+        "It will also help to solidify my knowledge of Firebase a bit more!",
+      completed: false
     },
     {
       project_id: 2,
       description:
-        "I need to set up my React application initially and test that I can display my data from the Firebase server"
+        "I need to set up my React application initially and test that I can display my data from the Firebase server",
+      completed: false
     }
   ]);
 };
